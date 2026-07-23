c57094c1e40a62a944d7c5c4b9ef2491b8690e90
feliprow <felipeneresvieira@gmail.com>
2026-07-23 19:40:03 -0300

    feat: Adiciona JPA ao NinjaRepository.

--- src/main/java/dev/feliprow/CadastroDeNinjas/Ninjas/NinjaRepository.java
+++ src/main/java/dev/feliprow/CadastroDeNinjas/Ninjas/NinjaRepository.java
@@ -1,4 +1,8 @@
 package dev.feliprow.CadastroDeNinjas.Ninjas;
 
-public class NinjaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+//JpaRepository<Classe que vai ser escaneada, Tipo do id (pk)>
+
+//JPA - simplificador para banco de dados
+public interface NinjaRepository extends JpaRepository<NinjaModel, Long> {
 }

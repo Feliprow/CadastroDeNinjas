f9d3293b1a695d6e72b5129e03bfd437ecec4764
feliprow <felipeneresvieira@gmail.com>
2026-07-28 18:55:04 -0300

    Adiciona endpoints na classe ninjaControler.

--- src/main/java/dev/feliprow/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/feliprow/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -8,11 +8,40 @@ public class NinjaController {
 
 //    @PostMapping Enviar informações
 //    @PutMapping Alterar informações
-//    @PatchMapping Alterar informações
-//    @DeleteMapping Deletar informações.
+    //    @PatchMapping Alterarq informações.
     @GetMapping("/boasvindas")// puxar informações
     public String boasVindas() {
         return "Essa é minha primeira menssagem nessa rota";
     }
 
+    //    ADICIONAR NINJA (create)
+    @PostMapping("/criar")
+    public String criarNinja() {
+        return "Ninja Criado";
+    }
+
+    //    MOSTRAR NINJAS (READ)
+    @GetMapping("/todos")
+    public String mostarNinjas() {
+        return "Ninja Criado";
+    }
+
+    //    PROCURAR NINJA POR ID (READ )
+    @GetMapping("/todos")
+    public String mostrarPorId() {
+        return "Ninja Criado";
+    }
+
+//    ALTERAR DADOS DOS NINJAS (UPDATE)
+    @PutMapping("/alterarID")
+    public String alterarNinja(){
+        return "ninja alterado";
+    }
+
+
+//    DELETAR NINJA (DELETE)
+    @DeleteMapping("/deletarID")
+    public String deletarNinjaPorId(){
+        return "Ninja deletado";
+    }
 }

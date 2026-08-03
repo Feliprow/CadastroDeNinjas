295d083640cd17554ae2630e9123d903343a3694
feliprow <felipeneresvieira@gmail.com>
2026-08-02 22:59:16 -0300

    feat: Adiciona funcionalidade de listar missões.

--- src/main/java/dev/feliprow/CadastroDeNinjas/Missoes/MissaoService.java
+++ src/main/java/dev/feliprow/CadastroDeNinjas/Missoes/MissaoService.java
@@ -1,4 +1,21 @@
 package dev.feliprow.CadastroDeNinjas.Missoes;
 
+import dev.feliprow.CadastroDeNinjas.Ninjas.NinjaModel;
+
+import java.util.List;
+import java.util.Optional;
+
 public class MissaoService {
+
+    private MissaoRepository missaoRepository;
+
+    public MissaoService(MissaoRepository missaoRepository) {
+        this.missaoRepository = missaoRepository;
+    }
+
+    //Listar missoes
+    public List<MissaoModel> listarMissoes() {
+        return missaoRepository.findAll();
+    }
+
 }
